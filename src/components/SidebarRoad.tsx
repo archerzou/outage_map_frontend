5af99cabbf9b0a085a8e2840172756928eea42be
Zou Zhefeng <archer.zou84@gmail.com>
2025-12-04 23:25:29 +1300

    add sample road closure data

--- src/components/SidebarRoad.tsx
+++ src/components/SidebarRoad.tsx
@@ -1,6 +1,3 @@
-import { Card, Text, TextInput, Button, Stack, Checkbox } from '@mantine/core'
-import { IconMapPin } from '@tabler/icons-react'
-import { useState } from 'react'
 import SidebarLayout from './shared/SidebarLayout'
 
 interface SidebarRoadProps {
@@ -9,38 +6,10 @@ interface SidebarRoadProps {
 }
 
 const SidebarRoad = ({ eventTypeName, onBack }: SidebarRoadProps) => {
-  const [locationSearch, setLocationSearch] = useState('Christchurch, New Zealand')
 
   return (
     <SidebarLayout eventTypeName={eventTypeName} onBack={onBack}>
-      <Card withBorder>
-        <Stack gap="sm">
-          <Text size="sm" fw={600}>Enter a location:</Text>
-
-          <TextInput
-            value={locationSearch}
-            onChange={(event) => setLocationSearch(event.currentTarget.value)}
-            rightSection={<IconMapPin size={16} />}
-          />
-
-          <Button fullWidth color="yellow" style={{ color: '#000' }}>
-            Find
-          </Button>
-        </Stack>
-      </Card>
-
-      <Card withBorder>
-        <Stack gap="sm">
-          <Text size="sm" fw={600}>Options</Text>
-
-          <Checkbox label="Incidents" defaultChecked />
-          <Checkbox label="Roadworks" defaultChecked />
-          <Checkbox label="Congestion" defaultChecked />
-          <Checkbox label="Road closures" defaultChecked />
-          <Checkbox label="Lane closed" defaultChecked />
-          <Checkbox label="Weather incidents" defaultChecked />
-        </Stack>
-      </Card>
+      <h1>Road</h1>
     </SidebarLayout>
   )
 }
